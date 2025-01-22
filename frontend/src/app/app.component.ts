a353820cd580e8e152c93def5007b01d1c9507c2
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-22 23:13:59 +0100

    feat(app): add toolbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { ButtonComponent } from './components/button/button.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
