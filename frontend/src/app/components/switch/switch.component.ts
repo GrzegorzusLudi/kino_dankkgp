f02b7a3ce68e5e9d626eee99aaf2364a08d552c3
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-23 00:11:29 +0100

    feat(switch): create component

---
+++ frontend/src/app/components/switch/switch.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch',
+  imports: [],
+  templateUrl: './switch.component.html',
+  styleUrl: './switch.component.scss',
+})
+export class SwitchComponent {}
