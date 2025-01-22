e5fd17a9e7b55bcf0704a6014149a8272ecd54b2
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-22 23:30:38 +0100

    feat(background): implement component structure and light and dark theme styles

--- frontend/src/app/components/background/background.component.ts
+++ frontend/src/app/components/background/background.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AsyncPipe,NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Theme } from '../../models/theme.enum';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-background',
-  imports: [],
+  imports: [NgIf, NgClass, AsyncPipe],
   templateUrl: './background.component.html',
-  styleUrl: './background.component.scss',
+  styleUrls: [
+    './background.light.component.scss',
+    './background.dark.component.scss',
+    './background.aero-light.component.scss',
+    './background.aero-dark.component.scss',
+  ],
 })
-export class BackgroundComponent {}
+export class BackgroundComponent implements OnInit {
+  protected theme!: Observable<Theme>;
+
+  constructor(private readonly themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.theme = this.themeService.getTheme();
+  }
+}
