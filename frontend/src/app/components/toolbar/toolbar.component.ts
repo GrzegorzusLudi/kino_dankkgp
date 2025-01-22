7c7d2a168efc0dda0fcec8d033b8d1bce93e1938
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-22 23:13:38 +0100

    feat(toolbar): basic toolbar look and feel in light and dark theme

--- frontend/src/app/components/toolbar/toolbar.component.ts
+++ frontend/src/app/components/toolbar/toolbar.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Theme } from '../../models/theme.enum';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [NgClass, NgIf, AsyncPipe],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss',
+  styleUrls: [
+    './toolbar.light.component.scss',
+    './toolbar.dark.component.scss',
+    './toolbar.aero-light.component.scss',
+    './toolbar.aero-dark.component.scss',
+  ],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent implements OnInit {
+  protected theme!: Observable<Theme>;
+
+  constructor(private readonly themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    this.theme = this.themeService.getTheme();
+  }
+}
