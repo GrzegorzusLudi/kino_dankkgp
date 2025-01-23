fcef0ad658bc0a66505d47ccd55da03ed11d65c5
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-23 20:41:37 +0100

    feat(switch): implement component, define structure, add logic, update styles

--- frontend/src/app/components/switch/switch.component.ts
+++ frontend/src/app/components/switch/switch.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-switch',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './switch.component.html',
   styleUrl: './switch.component.scss',
 })
-export class SwitchComponent {}
+export class SwitchComponent { // TODO Rename to toggle?
+  @Output() switch = new EventEmitter<boolean>();
+
+  emit(event: Event): void {
+    this.switch.next((event.target as HTMLInputElement)['checked']);
+  }
+}
