2bb07a786ddd0176c5736dd549b920473c4f3192
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-24 21:54:27 +0100

    feat(mode-toggle): create component

---
+++ frontend/src/app/components/mode-toggle/mode-toggle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mode-toggle',
+  imports: [],
+  templateUrl: './mode-toggle.component.html',
+  styleUrl: './mode-toggle.component.scss',
+})
+export class ModeToggleComponent {}
