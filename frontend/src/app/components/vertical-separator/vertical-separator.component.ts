c1f8fb0401da23ecea9571f1eee2cd5bc245e832
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-05 23:19:20 +0100

    feat(vertical-separator): implement component

--- frontend/src/app/components/vertical-separator/vertical-separator.component.ts
+++ frontend/src/app/components/vertical-separator/vertical-separator.component.ts
@@ -1,9 +1,17 @@
+import { AsyncPipe, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { ThemedDirective } from '../../directives/themed/themed.directive';
 
 @Component({
   selector: 'app-vertical-separator',
-  imports: [],
+  imports: [NgClass, NgIf, AsyncPipe],
   templateUrl: './vertical-separator.component.html',
-  styleUrl: './vertical-separator.component.scss',
+  styleUrls: [
+    './vertical-separator.light.component.scss',
+    './vertical-separator.dark.component.scss',
+    './vertical-separator.aero-light.component.scss',
+    './vertical-separator.aero-dark.component.scss',
+  ],
 })
-export class VerticalSeparatorComponent {}
+export class VerticalSeparatorComponent extends ThemedDirective {}
