3141b8d81b862fcc6de2db216e90ee75314787f2
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-05 23:01:11 +0100

    feat(vertical-separator): create component

---
+++ frontend/src/app/components/vertical-separator/vertical-separator.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerticalSeparatorComponent } from './vertical-separator.component';
+
+describe('VerticalSeparatorComponent', () => {
+  let component: VerticalSeparatorComponent;
+  let fixture: ComponentFixture<VerticalSeparatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VerticalSeparatorComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(VerticalSeparatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/components/vertical-separator/vertical-separator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-separator',
+  imports: [],
+  templateUrl: './vertical-separator.component.html',
+  styleUrl: './vertical-separator.component.scss',
+})
+export class VerticalSeparatorComponent {}
