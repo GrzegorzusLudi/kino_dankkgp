79eac2993f84950fbaf915ccc68dd266e1b7e9bd
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-05 23:50:25 +0100

    feat(user-info): user icon, displaying username

--- frontend/src/app/components/user-info/user-info.component.ts
+++ frontend/src/app/components/user-info/user-info.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgClass, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+import { ThemedDirective } from '../../directives/themed/themed.directive';
 
 @Component({
   selector: 'app-user-info',
-  imports: [],
+  imports: [NgIf, NgClass, AsyncPipe, FontAwesomeModule],
   templateUrl: './user-info.component.html',
-  styleUrl: './user-info.component.scss',
+  styleUrls: [
+    './user-info.light.component.scss',
+    './user-info.dark.component.scss',
+    './user-info.aero-light.component.scss',
+    './user-info.aero-dark.component.scss',
+  ],
 })
-export class UserInfoComponent {}
+export class UserInfoComponent extends ThemedDirective {
+  @Input() username = '';
+
+  faUser = faUser;
+}
