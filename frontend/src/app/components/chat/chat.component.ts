a660e3f5c31752115c8c6143f33987aface93de9
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-06 23:50:21 +0100

    feat(chat): header, list of messages, support for light & dark mode

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+import { ThemedDirective } from '../../directives/themed/themed.directive';
+import { Message } from '../../models/message.interface';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [NgClass, NgIf, AsyncPipe, NgFor],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss',
+  styleUrls: [
+    './chat.aero-light.component.scss',
+    './chat.aero-dark.component.scss',
+    './chat.dark.component.scss',
+    './chat.light.component.scss',
+  ],
 })
-export class ChatComponent {}
+export class ChatComponent extends ThemedDirective {
+  @Input() messages: Message[] = [];
+}
