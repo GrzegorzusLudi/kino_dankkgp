00cd7c0587d5028db3081c3d4e842ea7a0e6a797
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-06 23:52:42 +0100

    feat(app): display connected users info & chat with example test data

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 
 import { BackgroundComponent } from './components/background/background.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ConnectedUsersInfoComponent } from './components/connected-users-info/connected-users-info.component';
 import { ModeToggleComponent } from './components/mode-toggle/mode-toggle.component';
 import { TitleComponent } from './components/title/title.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { UserInfoComponent } from './components/user-info/user-info.component';
 import { VerticalSeparatorComponent } from './components/vertical-separator/vertical-separator.component';
 import { ThemedDirective } from './directives/themed/themed.directive';
+import { Message } from './models/message.interface';
 
 @Component({
   selector: 'app-root',
@@ -23,10 +26,31 @@ import { ThemedDirective } from './directives/themed/themed.directive';
     ModeToggleComponent,
     UserInfoComponent,
     VerticalSeparatorComponent,
+    ConnectedUsersInfoComponent,
+    ChatComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent extends ThemedDirective {
   title = 'Kino DANKKGP';
+  usernames = [
+    'Adam',
+    'Celina',
+    'John',
+    'Bertha',
+    'Robert',
+    'Anna',
+    'Łukasz',
+    'Przemek',
+    'Grzesiek',
+    'Kazik',
+  ];
+  messages: Message[] = [
+    {
+      date: new Date(),
+      username: 'Adam',
+      text: 'Hello World',
+    },
+  ];
 }
