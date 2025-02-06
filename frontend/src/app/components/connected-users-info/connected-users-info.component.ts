9fbd1a53af61f99e704c41f593f4fad1cb956533
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-06 23:15:08 +0100

    feat(connected-users-info): implement component

--- frontend/src/app/components/connected-users-info/connected-users-info.component.ts
+++ frontend/src/app/components/connected-users-info/connected-users-info.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+import { ThemedDirective } from '../../directives/themed/themed.directive';
 
 @Component({
   selector: 'app-connected-users-info',
-  imports: [],
+  imports: [NgClass, NgIf, NgFor, AsyncPipe],
   templateUrl: './connected-users-info.component.html',
-  styleUrl: './connected-users-info.component.scss',
+  styleUrls: [
+    './connected-users-info.light.component.scss',
+    './connected-users-info.dark.component.scss',
+    './connected-users-info.aero-light.component.scss',
+    './connected-users-info.aero-dark.component.scss',
+  ],
 })
-export class ConnectedUsersInfoComponent {}
+export class ConnectedUsersInfoComponent extends ThemedDirective {
+  @Input() usernames: string[] = [];
+}
