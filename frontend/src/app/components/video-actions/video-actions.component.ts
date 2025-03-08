274a3d83e5a5a8c177f4e62176b913dae1b3735d
Krzysztof Pająk <chris.araneo@gmail.com>
2025-03-08 14:04:18 +0100

    feat(video-actions): basic video actions structure and styles

--- frontend/src/app/components/video-actions/video-actions.component.ts
+++ frontend/src/app/components/video-actions/video-actions.component.ts
@@ -1,9 +1,17 @@
+import { AsyncPipe, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { ThemedDirective } from '../../directives/themed/themed.directive';
 
 @Component({
   selector: 'app-video-actions',
-  imports: [],
+  imports: [NgIf, NgClass, AsyncPipe],
   templateUrl: './video-actions.component.html',
-  styleUrl: './video-actions.component.scss',
+  styleUrls: [
+    './video-actions.aero-dark.component.scss',
+    './video-actions.aero-light.component.scss',
+    './video-actions.dark.component.scss',
+    './video-actions.light.component.scss',
+  ],
 })
-export class VideoActionsComponent {}
+export class VideoActionsComponent extends ThemedDirective {}
