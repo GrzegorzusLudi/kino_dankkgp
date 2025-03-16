af071bb4b6ff734efb029e8d45459a7beb90ac32
Krzysztof Pająk <chris.araneo@gmail.com>
2025-03-16 20:20:58 +0100

    feat(input): implement basic input logic and structure

--- frontend/src/app/components/input/input.component.ts
+++ frontend/src/app/components/input/input.component.ts
@@ -1,9 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   imports: [],
   templateUrl: './input.component.html',
-  styleUrl: './input.component.scss',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {}
+export class InputComponent implements ControlValueAccessor {
+  value: string;
+  isDisabled: boolean;
+
+  changed?: (value: string) => void;
+  touched?: () => void;
+
+  constructor() {
+    this.value = '';
+    this.isDisabled = false;
+  }
+
+  onChange(event: Event): void {
+    const value = (event['target'] as HTMLInputElement)['value'];
+
+    if (this.changed) {
+      this.changed(value || '');
+    }
+  }
+
+  onBlur(): void {
+    if (this.touched) {
+      this.touched();
+    }
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: () => void): void {
+    this.changed = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.touched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+}
