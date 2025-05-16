173fa677828b654a72ebd78937778382699a34e1
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-16 21:06:30 +0200

    feat(video-actions): use InputComponent

--- frontend/src/app/components/video-actions/video-actions.component.ts
+++ frontend/src/app/components/video-actions/video-actions.component.ts
@@ -2,10 +2,11 @@ import { AsyncPipe, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { ThemedDirective } from '../../directives/themed/themed.directive';
+import { InputComponent } from '../input/input.component';
 
 @Component({
   selector: 'app-video-actions',
-  imports: [AsyncPipe, NgClass, NgIf],
+  imports: [AsyncPipe, InputComponent, NgClass, NgIf],
   templateUrl: './video-actions.component.html',
   styleUrls: [
     './video-actions.aero-dark.component.scss',
