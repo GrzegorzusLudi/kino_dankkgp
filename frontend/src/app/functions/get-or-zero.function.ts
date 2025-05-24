2c21e958be0900ac15ef02b16160d49e0da15db6
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-24 17:51:13 +0200

    feat(get-or-zero): create function

--- frontend/src/app/functions/get-or-zero.function.ts
+++ frontend/src/app/functions/get-or-zero.function.ts
@@ -1,13 +1,11 @@
 import { get, isNaN } from 'lodash';
 
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
 export const getOrZero = (object: object | null, path: string): number => {
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-  const result = Number(get(object, path) || 0);
+  const result = Number(get(object, path));
 
   if (isNaN(result)) {
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     return 0;
   }
 
