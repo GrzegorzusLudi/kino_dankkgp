32c7551a54a766cae5b088c24b1e7ff7379a94da
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-24 22:46:10 +0200

    feat: provide socket io

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideSocketIo } from 'ngx-socket-io';
 
 import { routes } from './app.routes';
 
@@ -7,5 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideSocketIo({ url: 'http://localhost:8988', options: {} }),
   ],
 };
