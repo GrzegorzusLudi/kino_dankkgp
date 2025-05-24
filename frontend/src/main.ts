14608b2ae20c8062fb878fcdded1655d795d57c3
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-24 18:12:33 +0200

    fix(main): dont use await, disable eslint rules

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable unicorn/prefer-top-level-await */
+
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-try {
-  await bootstrapApplication(AppComponent, appConfig);
-} catch (error: unknown) {
-  // eslint-disable-next-line no-console
-  console.error(error);
-}
+bootstrapApplication(AppComponent, appConfig).catch((error: unknown) =>
+  console.error(error),
+);
