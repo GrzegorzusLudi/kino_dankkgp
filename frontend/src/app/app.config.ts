311ef41e6a8e12f1efaaf124e23a25c1e55d8c04
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-25 14:35:04 +0200

    fix: remove socket io provider

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideSocketIo } from 'ngx-socket-io';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideSocketIo({ url: 'http://localhost:8988', options: {} }),
+    provideRouter(routes)
   ],
 };
