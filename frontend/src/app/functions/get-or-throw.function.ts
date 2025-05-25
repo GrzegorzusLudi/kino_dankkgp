6dde5590eb81209a169e803ce313e216fed4f1a6
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-25 14:34:16 +0200

    feat(get-or-throw): update param types

--- frontend/src/app/functions/get-or-throw.function.ts
+++ frontend/src/app/functions/get-or-throw.function.ts
@@ -1,15 +1,15 @@
 import { get, isUndefined } from 'lodash';
 
-export const getOrThrow = <TObject extends object, TKey extends keyof TObject>(
-  object: TObject,
-  path: keyof TKey,
-): TObject[TKey] => {
-  const result = get(object, path) as unknown;
+export const getOrThrow = <T, TKey extends keyof T>(
+  object: T,
+  path: TKey,
+): T[TKey] => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
+  const result = get(object, path) as unknown as T[TKey] | undefined;
 
   if (isUndefined(result)) {
     throw new Error(`Property ${String(path)} is undefined`);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
-  return result as TObject[TKey];
+  return result;
 };
