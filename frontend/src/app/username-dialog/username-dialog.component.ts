82a7111647e72cbc90689d59a47d80d8584678ec
Krzysztof Pająk <chris.araneo@gmail.com>
2025-05-30 21:08:54 +0200

    feat(username-dialog): create component

---
+++ frontend/src/app/username-dialog/username-dialog.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username-dialog',
+  imports: [],
+  templateUrl: './username-dialog.component.html',
+  styleUrl: './username-dialog.component.scss',
+})
+export class UsernameDialogComponent {}
