e78ca2a8805472bb54c7b69a459f17951261d078
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-02 20:15:44 +0200

    chore: remove todo comment

--- frontend/src/app/components/username-dialog/username-dialog.component.ts
+++ frontend/src/app/components/username-dialog/username-dialog.component.ts
@@ -42,7 +42,6 @@ export class UsernameDialogComponent extends ThemedDirective {
   }
 
   setUsername(): void {
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (this.username.value) {
       this.apiService.setUsername(this.username.value);
       this.dialogRef.close();
