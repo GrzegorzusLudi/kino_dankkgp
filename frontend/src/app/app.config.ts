8f83129d40b401573a7a2a2c3740154f25d00593
Krzysztof Pająk <chris.araneo@gmail.com>
2025-06-03 20:21:58 +0200

    feat: create socket injection token

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { io, Socket } from 'socket.io-client';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+
+export const SOCKET = new InjectionToken<Socket>('Socket');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: SOCKET, useValue: io(environment.api.url) },
   ],
 };
