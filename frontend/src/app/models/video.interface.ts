0cb313c9c7af1cd0f7be29e4b48edc29e9d63069
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-15 21:35:19 +0100

    feat(video): create interface

---
+++ frontend/src/app/models/video.interface.ts
@@ -0,0 +1,11 @@
+export interface Video {
+    url: string,
+    videoId: string,
+    title: string,
+    type: string,
+    user: {
+        nick: string,
+        num: number
+    }
+    'duration_in_seconds': number
+}
