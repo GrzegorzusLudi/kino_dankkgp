f2ecb466e43a0e3af792a3e9e69274f2ca3149e6
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-15 21:05:41 +0100

    feat(apikeytest): load key from .env

--- server/apikeytest.py
+++ server/apikeytest.py
@@ -1,7 +1,11 @@
+import os
 import re
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
-service = build('youtube', 'v3', developerKey='AIzaSyDAEkqCJsG3aUDTepgIHZIq2riYkPQaO-g')
+load_dotenv()
+
+service = build('youtube', 'v3', developerKey=os.getenv('GOOGLE_API_KEY'))
 
 result = service.videos().list(part='contentDetails',id='DQEHqbKAY20',maxResults=1).execute()
 
