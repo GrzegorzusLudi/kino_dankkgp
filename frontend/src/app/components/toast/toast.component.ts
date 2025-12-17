5dfd494770dc68c89621252ac2331b54be8e8302
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-17 20:58:39 +0100

    feat(toast): extend theme directive

--- frontend/src/app/components/toast/toast.component.ts
+++ frontend/src/app/components/toast/toast.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { ThemedDirective } from '../../directives/themed/themed.directive';
+import { ThemeService } from '../../services/theme/theme.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-toast',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgIf, AsyncPipe],
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.dark.component.scss', './toast.light.component.scss', './toast.aero-light.component.scss', './toast.aero-dark.component.scss'],
 })
-export class ToastComponent {
+export class ToastComponent extends ThemedDirective {
   @Input() title: string = '';
   @Input() message: string = '';
+
+  constructor(protected override readonly themeService: ThemeService) {
+    super(themeService);
+  }
 }
