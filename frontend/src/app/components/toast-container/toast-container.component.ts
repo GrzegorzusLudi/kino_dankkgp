e3359db79b63316e15726e5a0664b1c5ce5c365a
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-21 00:07:26 +0100

    feat(toast-container): display error toasts

--- frontend/src/app/components/toast-container/toast-container.component.ts
+++ frontend/src/app/components/toast-container/toast-container.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ToastComponent } from '../toast/toast.component';
+import { BehaviorSubject, Observable, Subscription, tap } from 'rxjs';
+import { Toast } from '../../models/toast.interface';
+import { ApiService } from '../../services/api/api.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-toast-container',
-  imports: [],
+  imports: [AsyncPipe, ToastComponent],
   templateUrl: './toast-container.component.html',
   styleUrl: './toast-container.component.scss',
 })
-export class ToastContainerComponent {}
+export class ToastContainerComponent implements OnInit, OnDestroy {
+  private toastsSubject = new BehaviorSubject<Toast[]>([]);
+  private subscription = new Subscription();
+
+  constructor(private readonly apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.apiService.error
+        .pipe(
+          tap((error) => {
+            if (error) {
+              this.toastsSubject.next([
+                ...this.toastsSubject.getValue(),
+                {
+                  id: crypto.randomUUID(),
+                  title: 'Error',
+                  message: error,
+                  variant: 'danger',
+                },
+              ]);
+            }
+          }),
+        )
+        .subscribe(),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  get toasts(): Observable<Toast[]> {
+    return this.toastsSubject.asObservable();
+  }
+
+  removeToast(id: string): void {
+    this.toastsSubject.next(
+      this.toastsSubject.getValue().filter((toast) => toast.id !== id),
+    );
+  }
+}
--- frontend/src/app/models/toast.interface.ts
+++ frontend/src/app/models/toast.interface.ts
@@ -1,4 +1,5 @@
 export interface Toast {
+  id: string;
   title: string;
   message: string;
   variant: 'info' | 'success' | 'danger';
