081182a7a153422e8ec21e2de69dadb1a1f01003
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-21 23:07:35 +0100

    feat(queue): extend themed directive, create theme stylesheets

--- frontend/src/app/components/queue/queue.component.ts
+++ frontend/src/app/components/queue/queue.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { ThemedDirective } from '../../directives/themed/themed.directive';
+import { ThemeService } from '../../services/theme/theme.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-queue',
-  imports: [],
+  imports: [AsyncPipe, NgIf],
   templateUrl: './queue.component.html',
-  styleUrl: './queue.component.scss',
+  styleUrls: [
+    './queue.aero-dark.component.scss',
+    './queue.aero-light.component.scss',
+    './queue.dark.component.scss',
+    './queue.light.component.scss',
+  ],
 })
-export class QueueComponent {}
+export class QueueComponent extends ThemedDirective {
+  constructor(protected override readonly themeService: ThemeService) {
+    super(themeService);
+  }
+}
