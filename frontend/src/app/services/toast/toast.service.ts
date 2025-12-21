0883f6de508d9f9d7e5465bf4d149de4092aa51b
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-21 22:13:50 +0100

    feat(toast): implement service

--- frontend/src/app/services/toast/toast.service.ts
+++ frontend/src/app/services/toast/toast.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Toast } from '../../models/toast.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToastService {
+  private readonly toastSubject = new Subject<Toast>();
 
-  constructor() { }
+  get get(): Observable<Toast> {
+    return this.toastSubject.asObservable();
+  }
+
+  next(toast: Toast): void {
+    this.toastSubject.next(toast);
+  }
 }
