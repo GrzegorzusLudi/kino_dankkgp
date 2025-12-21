18504e0e90a389bed46f3e58e98d736caa339b04
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-21 23:02:43 +0100

    feat(queue): create component

---
+++ frontend/src/app/components/queue/queue.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-queue',
+  imports: [],
+  templateUrl: './queue.component.html',
+  styleUrl: './queue.component.scss',
+})
+export class QueueComponent {}
