afc09e7f066557c7f5eb7c77abaa7d7edf15293c
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-22 18:47:38 +0100

    feat(round-button): implement base structure, logic and styles

--- frontend/src/app/components/round-button/round-button.component.ts
+++ frontend/src/app/components/round-button/round-button.component.ts
@@ -1,9 +1,24 @@
+import { AsyncPipe, NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { ThemedDirective } from '../../directives/themed/themed.directive';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-round-button',
-  imports: [],
+  imports: [AsyncPipe, NgClass, NgIf],
   templateUrl: './round-button.component.html',
-  styleUrl: './round-button.component.scss',
+  styleUrls: [
+    './round-button.aero-dark.component.scss',
+    './round-button.aero-light.component.scss',
+    './round-button.dark.component.scss',
+    './round-button.light.component.scss',
+  ],
 })
-export class RoundButtonComponent {}
+export class RoundButtonComponent extends ThemedDirective {
+  protected clicked = false;
+
+  constructor(protected override readonly themeService: ThemeService) {
+    super(themeService);
+  }
+}
