8f924bf8b1bc2254dbd3279bc422e78959a0dbe5
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-22 18:32:56 +0100

    feat(round-button): create component

---
+++ frontend/src/app/components/round-button/round-button.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-round-button',
+  imports: [],
+  templateUrl: './round-button.component.html',
+  styleUrl: './round-button.component.scss',
+})
+export class RoundButtonComponent {}
