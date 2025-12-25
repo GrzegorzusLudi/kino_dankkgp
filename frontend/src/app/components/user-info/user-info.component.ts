b41f617e37227184f285ceb5bfb87bdf8f01b75f
Krzysztof Pająk <chris.araneo@gmail.com>
2025-12-25 16:31:40 +0100

    feat(user-info): open username dialog on button click

--- frontend/src/app/components/user-info/user-info.component.ts
+++ frontend/src/app/components/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { AsyncPipe, NgClass, NgIf } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
@@ -6,10 +6,18 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { ThemedDirective } from '../../directives/themed/themed.directive';
 import { ThemeService } from '../../services/theme/theme.service';
 import { RoundButtonComponent } from '../round-button/round-button.component';
+import { MatDialog } from '@angular/material/dialog';
+import { UsernameDialogComponent } from '../username-dialog/username-dialog.component';
+import {
+  USERNAME_DIALOG_ENTER_ANIMATION_DURATION,
+  USERNAME_DIALOG_EXIT_ANIMATION_DURATION,
+  USERNAME_DIALOG_HEIGHT,
+  USERNAME_DIALOG_WIDTH,
+} from '../username-dialog/username-dialog.consts';
 
 @Component({
   selector: 'app-user-info',
-  imports: [AsyncPipe, FontAwesomeModule, NgClass, NgIf, RoundButtonComponent],
+  imports: [FontAwesomeModule, NgClass, RoundButtonComponent],
   templateUrl: './user-info.component.html',
   styleUrls: [
     './user-info.aero-dark.component.scss',
@@ -23,7 +31,19 @@ export class UserInfoComponent extends ThemedDirective {
 
   faUser = faUser;
 
-  constructor(protected override readonly themeService: ThemeService) {
+  constructor(
+    protected override readonly themeService: ThemeService,
+    public dialog: MatDialog,
+  ) {
     super(themeService);
+  }
+
+  openUsernameDialog(): void {
+    this.dialog.open(UsernameDialogComponent, {
+      width: USERNAME_DIALOG_WIDTH,
+      height: USERNAME_DIALOG_HEIGHT,
+      enterAnimationDuration: USERNAME_DIALOG_ENTER_ANIMATION_DURATION,
+      exitAnimationDuration: USERNAME_DIALOG_EXIT_ANIMATION_DURATION,
+    });
   }
 }
