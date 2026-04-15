a52ae4644a67bb79ae087a84227275f71f5e30dc
Krzysztof Pająk <chris.araneo@gmail.com>
2026-04-15 22:02:46 +0200

    style: format code

--- frontend/libs/button/src/lib/button.component.ts
+++ frontend/libs/button/src/lib/button.component.ts
@@ -4,23 +4,18 @@ import { Component, Input } from '@angular/core';
 import { ThemedDirective, ThemeService } from 'theme';
 
 @Component({
-  selector: 'lib-button',
-  imports: [NgClass],
-  templateUrl: './button.component.html',
-  styleUrls: [
-    './button.aero-dark.component.scss',
-    './button.aero-light.component.scss',
-    './button.dark.component.scss',
-    './button.light.component.scss',
-  ],
+    selector: 'lib-button',
+    imports: [NgClass],
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.aero-dark.component.scss', './button.aero-light.component.scss', './button.dark.component.scss', './button.light.component.scss']
 })
 export class ButtonComponent extends ThemedDirective {
-  @Input() variant: 'primary' | 'ghost' = 'primary';
-  @Input() height: 'small' | 'medium' = 'medium';
+    @Input() variant: 'primary' | 'ghost' = 'primary';
+    @Input() height: 'small' | 'medium' = 'medium';
 
-  protected clicked = false;
+    protected clicked = false;
 
-  constructor(protected override readonly themeService: ThemeService) {
-    super(themeService);
-  }
+    constructor(protected override readonly themeService: ThemeService) {
+        super(themeService);
+    }
 }
