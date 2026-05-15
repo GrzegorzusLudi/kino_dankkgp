280036636072a28b301a56ac3f7214cb234f7946
Krzysztof Pająk <chris.araneo@gmail.com>
2026-05-15 18:58:51 +0200

    fix: remove conflicting storybook plugin

--- frontend/libs/button/.storybook/vite.config.ts
+++ frontend/libs/button/.storybook/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import angular from '@analogjs/vite-plugin-angular';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -9,7 +8,6 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [
-    angular({ tsconfig: resolve(__dirname, 'tsconfig.json') }),
     nxViteTsPaths(),
   ],
   define: {
