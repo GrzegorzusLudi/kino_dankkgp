51d3795cce7b1c120fc8dd31de37a6d59ff48a4c
Krzysztof Pająk <chris.araneo@gmail.com>
2026-05-17 14:19:23 +0200

    refactor(button): use hostDirectives property

--- frontend/libs/button/src/lib/button.component.ts
+++ frontend/libs/button/src/lib/button.component.ts
@@ -5,12 +5,10 @@ import { ThemedDirective } from 'theme';
 @Component({
   selector: 'lib-button',
   templateUrl: './button.component.html',
-  styleUrls: [
-    './button.aero.component.scss',
-    './button.flat.component.scss',
-  ],
+  styleUrls: ['./button.aero.component.scss', './button.flat.component.scss'],
+  hostDirectives: [ThemedDirective],
 })
-export class ButtonComponent extends ThemedDirective {
+export class ButtonComponent {
   @Input() variant: 'primary' | 'ghost' = 'primary';
   @Input() height: 'small' | 'medium' = 'medium';
 
