1de2aec141e19de4634a0ef8de8d0ae1c1d06ac0
Krzysztof Pająk <chris.araneo@gmail.com>
2026-05-17 16:17:13 +0200

    chore(title): delete unused imports

--- frontend/libs/title/src/lib/title.component.ts
+++ frontend/libs/title/src/lib/title.component.ts
@@ -1,11 +1,9 @@
-import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { ThemedDirective } from 'theme';
 
 @Component({
   selector: 'lib-title',
-  imports: [NgClass],
   templateUrl: './title.component.html',
   styleUrls: [
     './title.flat.component.scss',
