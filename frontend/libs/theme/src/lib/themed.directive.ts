cbda7893c92c0f4a380f8a7d64cab1aeede10c82
Krzysztof Pająk <chris.araneo@gmail.com>
2026-05-17 11:42:57 +0200

    feat(themed): make directive apply theme class to first child

--- frontend/libs/theme/src/lib/themed.directive.ts
+++ frontend/libs/theme/src/lib/themed.directive.ts
@@ -1,8 +1,31 @@
-import { Directive, inject, Signal } from '@angular/core';
+import { Directive, effect, ElementRef, inject, Renderer2, Signal } from '@angular/core';
 
 import { THEME } from './theme.token';
 
 @Directive()
 export class ThemedDirective {
   protected readonly theme: Signal<string> = inject(THEME);
+
+  private readonly elementRef = inject(ElementRef);
+  private readonly renderer = inject(Renderer2);
+
+  private previousTheme: string | null = null;
+
+  constructor() {
+    effect(() => {
+      const theme = this.theme();
+      const firstChild = this.elementRef.nativeElement.firstElementChild;
+      
+      if (!firstChild) {
+        return;
+      }
+
+      if (this.previousTheme) {
+        this.renderer.removeClass(firstChild, this.previousTheme);
+      }
+
+      this.renderer.addClass(firstChild, theme);
+      this.previousTheme = theme;
+    });
+  }
 }
