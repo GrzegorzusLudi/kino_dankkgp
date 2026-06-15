5739003ce31a26f5ed30b7c45bdfb6144221d951
Krzysztof Pająk <chris.araneo@gmail.com>
2026-06-15 20:28:36 +0200

    feat(action): update enum and add MoveVideoUp and SkipVideo

--- frontend/apps/browser/src/app/models/action.enum.ts
+++ frontend/apps/browser/src/app/models/action.enum.ts
@@ -2,4 +2,6 @@ export enum Action {
   SetUsername = 'set-nick',
   SendMessage = 'chat-message',
   AddVideo = 'queue-add-video',
+  MoveVideoUp = 'queue-move-up-video',
+  SkipVideo = 'queue-skip-video',
 }
