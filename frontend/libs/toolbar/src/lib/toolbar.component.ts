25ba33abee78482fd0d53d9649720a042de1ebd9
Krzysztof Pająk <chris.araneo@gmail.com>
2026-06-17 21:00:45 +0200

    style: format code

--- frontend/libs/toolbar/src/lib/toolbar.component.ts
+++ frontend/libs/toolbar/src/lib/toolbar.component.ts
@@ -5,10 +5,7 @@ import { ThemedDirective } from 'theme';
 @Component({
   selector: 'lib-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: [
-    './toolbar.aero.component.scss',
-    './toolbar.flat.component.scss',
-  ],
+  styleUrls: ['./toolbar.aero.component.scss', './toolbar.flat.component.scss'],
   hostDirectives: [ThemedDirective],
 })
 export class ToolbarComponent {}
