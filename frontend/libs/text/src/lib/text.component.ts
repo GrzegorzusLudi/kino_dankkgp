b52f14d1a76d3214f497babf1539b138c0243f4b
Krzysztof Pająk <chris.araneo@gmail.com>
2026-06-24 20:40:13 +0200

    style(text): format code, organize imports, organize json

--- frontend/libs/text/src/lib/text.component.ts
+++ frontend/libs/text/src/lib/text.component.ts
@@ -5,11 +5,8 @@ import { ThemedDirective } from 'theme';
 @Component({
   selector: 'lib-text',
   templateUrl: './text.component.html',
-  styleUrls: [
-    './text.flat.component.scss',
-    './text.aero.component.scss',
-  ],
-  hostDirectives: [ThemedDirective]
+  styleUrls: ['./text.flat.component.scss', './text.aero.component.scss'],
+  hostDirectives: [ThemedDirective],
 })
 export class TextComponent {
   variant = input<'info' | 'success' | 'danger'>();
