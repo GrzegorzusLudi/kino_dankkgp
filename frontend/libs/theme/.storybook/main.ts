0a5e887d9186e108556882a5fcef50260fc900be
Krzysztof Pająk <chris.araneo@gmail.com>
2026-06-29 07:36:07 +0200

    chore(theme): update lib config, add storybook config

---
+++ frontend/libs/theme/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.ts'],
+  addons: [],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  core: {
+    builder: {
+      name: '@storybook/builder-vite',
+      options: {
+        viteConfigPath: '.storybook/vite.config.ts',
+      },
+    },
+  },
+};
+
+export default config;
