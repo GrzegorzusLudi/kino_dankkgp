ebf99e93b3dee1334b607ae6b8f9f8a04e5142f3
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-01 23:26:10 +0200

    style(duration): add minor stylistic change

--- frontend/apps/browser/src/app/pipes/duration/duration.pipe.ts
+++ frontend/apps/browser/src/app/pipes/duration/duration.pipe.ts
@@ -1,6 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { match, P } from 'ts-pattern';
 
+const { nullish, number } = P;
+
 @Pipe({
   name: 'duration',
   standalone: true,
@@ -8,8 +10,8 @@ import { match, P } from 'ts-pattern';
 export class DurationPipe implements PipeTransform {
   transform(seconds: number | null | undefined): string {
     return match(seconds)
-      .with(P.nullish, () => '0:00')
-      .with(P.number.lt(1), () => '0:00')
+      .with(nullish, () => '0:00')
+      .with(number.lt(1), () => '0:00')
       .otherwise(
         (value) =>
           `${Math.floor(value / 60)}:${Math.floor(value % 60)
