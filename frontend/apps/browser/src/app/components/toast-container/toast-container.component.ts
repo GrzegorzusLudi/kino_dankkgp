e0fbd955500729420aff1717624263bcfc2a5c51
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-01 20:29:24 +0200

    refactor(toast-container): use inject

--- frontend/apps/browser/src/app/components/toast-container/toast-container.component.ts
+++ frontend/apps/browser/src/app/components/toast-container/toast-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ToastComponent } from '../toast/toast.component';
 import { Toast } from '../../models/toast.interface';
@@ -13,8 +13,10 @@ import { ToastService } from '../../services/toast/toast.service';
 export class ToastContainerComponent {
   readonly toasts = signal<Toast[]>([]);
 
-  constructor(toastService: ToastService) {
-    toastService.get.pipe(takeUntilDestroyed()).subscribe((toast) => {
+  private readonly toastService = inject(ToastService);
+
+  constructor() {
+    this.toastService.get.pipe(takeUntilDestroyed()).subscribe((toast) => {
       this.toasts.update((current) => [...current, toast]);
     });
   }
