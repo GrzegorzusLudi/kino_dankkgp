13b0b57a93c6ffde0f0b7ba2f4672f9d565d2669
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-01 22:31:44 +0200

    refactor(username-dialog): use pattern matching

--- frontend/apps/browser/src/app/components/username-dialog/username-dialog.component.ts
+++ frontend/apps/browser/src/app/components/username-dialog/username-dialog.component.ts
@@ -2,12 +2,16 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { take } from 'rxjs';
+import { match, P } from 'ts-pattern';
+import { noop } from 'lodash-es';
 
 import { ThemedDirective } from 'theme';
 import { ApiService } from '../../services/api/api.service';
 import { ButtonComponent } from 'button';
 import { HeaderComponent } from 'header';
 import { InputComponent } from 'input';
+
+const { nullish, union } = P;
 
 @Component({
   selector: 'app-username-dialog',
@@ -38,9 +42,11 @@ export class UsernameDialogComponent implements OnInit {
   }
 
   setUsername(): void {
-    if (this.username.value) {
-      this.apiService.setUsername(this.username.value);
-      this.dialogRef.close();
-    }
+    match(this.username.value)
+      .with(union(nullish, ''), noop)
+      .otherwise((value) => {
+        this.apiService.setUsername(value);
+        this.dialogRef.close();
+      });
   }
 }
